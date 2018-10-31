e6e850fa77ea77c0c5d2721c7fdb12c62ad712d2
danilovalerio <danilo_vs@hotmail.com>
2018-10-31 10:50:16 -0300

    Exemplo 2 Union(addAll)-Intersection(retainAll)-Difference(removeAll)

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,7 +1,9 @@
 package aplicacao;
 
+import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Programa {
 
@@ -34,13 +36,58 @@ public class Programa {
 		//remove x tal que a primeira letra seja T
 		set.removeIf(x -> x.charAt(0) == 'T'); //remove todos que começam com 'T'
 		
+		/*ComentadoParaTeste do relacaoDeConjuntos
+		 
 		System.out.println(set.contains("TV")+"\n");
 		
 		for(String item : set) {
 			System.out.println(item);
 		}
 		
+		*/
+		
+		relacaoDeConjuntos();
 
+	}
+	
+	
+	public static void relacaoDeConjuntos() {
+		
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(1,3,5,6,7,8,9,10));
+		
+		System.out.println(" Conjunto A");
+		for(Integer i : a) {
+			System.out.print(" "+i);
+		}
+		
+		System.out.println("\n Conjunto B");
+		for(Integer i : b) {
+			System.out.print(" "+i);
+		}
+		
+		System.out.println("\n----------------------------------------------");
+
+		//union - cria um c que será cópia do a
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.print(" Union: "+c);
+
+
+		//intersection - será os valores que são em comuns em ambos os conjuntos
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.print("\n Intersection: "+d);
+
+		//difference - será os valores que são diferentes
+		Set<Integer> e = new TreeSet<>(a);
+		c.removeAll(b);
+		System.out.print("\n Difference: "+e);
+
+		
+		
+		
+		
 	}
 
 }
